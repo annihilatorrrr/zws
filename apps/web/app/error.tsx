7959deb42227714573959cebc354e773c1747184
Jonah Snider <jonah@jonahsnider.com>
2026-04-14 00:59:02 -0700

    feat(web): remove highlight.io error tracking
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- apps/web/app/error.tsx
+++ apps/web/app/error.tsx
@@ -1,19 +1,9 @@
 'use client';
 
-import { H } from '@highlight-run/next/client';
-import { useEffect } from 'react';
 import { Button } from './components/button';
 import { H1 } from './components/headings/h1';
 
-const SHOULD_REPORT_ERROR = process.env.NODE_ENV !== 'development';
-
-export default function ErrorPage({ error, reset }: { error: Error; reset: () => void }) {
-	useEffect(() => {
-		if (SHOULD_REPORT_ERROR) {
-			H.consumeError(error);
-		}
-	}, [error]);
-
+export default function ErrorPage({ reset }: { error: Error; reset: () => void }) {
 	return (
 		<main className='flex flex-col items-center justify-center gap-4 sm:pt-16 md:pt-36'>
 			<H1>An unexpected error occurred</H1>
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,3 @@
-import { HighlightInit } from '@highlight-run/next/client';
 import { Analytics } from '@vercel/analytics/react';
 import clsx from 'clsx';
 import type { Metadata, Viewport } from 'next';
@@ -37,40 +36,28 @@ const inter = Lato({ weight: ['400', '700'], subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<>
-			<HighlightInit
-				projectId={process.env.NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID}
-				tracingOrigins={true}
-				excludedHostnames={['localhost']}
-				networkRecording={{
-					enabled: true,
-					recordHeadersAndBody: true,
-				}}
-			/>
+		<html lang='en'>
+			<head>
+				<PlausibleProvider enabled={true} src='https://plausible.io/js/pa-XajU-qmMJYrpEgDCnYn8i.js' />
+			</head>
+			<body className={clsx(inter.className)}>
+				<TrpcProvider>
+					<Wave />
 
-			<html lang='en'>
-				<head>
-					<PlausibleProvider enabled={true} src='https://plausible.io/js/pa-XajU-qmMJYrpEgDCnYn8i.js' />
-				</head>
-				<body className={clsx(inter.className)}>
-					<TrpcProvider>
-						<Wave />
-
-						<div className='min-h-screen'>
-							<Navbar />
-							<DotGrid height={1} className='left-0 max-xl:top-[550px] max-md:invisible xl:top-[787px]' />
-							<DotGrid height={2} className='top-[145px] right-0' />
-							<LightSpot className='-z-50 translate-y-[25px] transform max-2xl:translate-x-[50px] max-md:h-0 max-md:w-0 2xl:translate-x-[300px]' />
-							<div className='container mx-auto max-w-(--breakpoint-xl) px-4'>
-								{children}
-								<Footer />
-							</div>
+					<div className='min-h-screen'>
+						<Navbar />
+						<DotGrid height={1} className='left-0 max-xl:top-[550px] max-md:invisible xl:top-[787px]' />
+						<DotGrid height={2} className='top-[145px] right-0' />
+						<LightSpot className='-z-50 translate-y-[25px] transform max-2xl:translate-x-[50px] max-md:h-0 max-md:w-0 2xl:translate-x-[300px]' />
+						<div className='container mx-auto max-w-(--breakpoint-xl) px-4'>
+							{children}
+							<Footer />
 						</div>
-					</TrpcProvider>
+					</div>
+				</TrpcProvider>
 
-					<Analytics />
-				</body>
-			</html>
-		</>
+				<Analytics />
+			</body>
+		</html>
 	);
 }
