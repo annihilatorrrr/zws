c35608e6637a5bbeb39c7b36745af81118ce9697
Jonah Snider <jonah@jonahsnider.com>
2026-04-16 14:06:31 -0700

    build(web): remove env var mapping in next.config.ts
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from 'next';
 
 export default {
-	env: {
-		NEXT_PUBLIC_WEBSITE_URL: process.env.WEBSITE_URL,
-		NEXT_PUBLIC_API_URL: process.env.API_URL,
-	},
 	productionBrowserSourceMaps: true,
 	redirects: async () => [
 		{
